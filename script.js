644eb1fd465e53094c298742d45fb52ad3570a8a
lasteChomp <163031341+lasteChomp@users.noreply.github.com>
2025-11-14 18:06:30 +0300

    feat(script.js): add a feature that recreates grid from scratch with users' prompt
    - Added a button that prompts the user a number up to 100.
    - Divided the code into functions; createGrid, drawOnGrid, playGrid.

--- script.js
+++ script.js
@@ -1,43 +1,72 @@
 const container = document.querySelector(".container");
-const heightSize = container.clientHeight / 16;
-const widthSize = heightSize * 16;
-console.log(heightSize);
-console.log(widthSize);
+const buttonDiv = document.createElement("div");
+const button = document.createElement("button");
+button.textContent = "New Grid";
+buttonDiv.appendChild(button);
+document.body.prepend(buttonDiv);
 
-// First, create 16 rows
-for (let i = 0; i < 16; i++) {
-  const row = document.createElement("div");
-  row.className = "row";
-  row.style.width = `${widthSize}px`;
-  row.style.height = `${heightSize}px`;
-  row.style.display = "flex";
-  container.appendChild(row);
-}
+let gridNum = 16;
 
-const rows = document.querySelectorAll(".row");
+const createGrid = (gridNum) => {
+  const heightSize = container.clientHeight / gridNum;
+  const widthSize = heightSize * gridNum;
 
-// Create 16 squares in each row
-for (const row of rows) {
-  const widthSize = row.clientWidth / 16;
-  for (let i = 0; i < 16; i++) {
-    const square = document.createElement("div");
-    square.className = "square";
-    square.style.width = `${widthSize}px`;
-    square.style.height = "100%";
-    row.appendChild(square);
+  // First, create 16 rows
+  for (let i = 0; i < gridNum; i++) {
+    const row = document.createElement("div");
+    row.className = "row";
+    row.style.width = `${widthSize}px`;
+    row.style.height = `${heightSize}px`;
+    row.style.display = "flex";
+    container.appendChild(row);
   }
-}
 
-const squares = document.querySelectorAll(".square");
+  const rows = document.querySelectorAll(".row");
 
-squares.forEach((square) => {
-  square.addEventListener("mouseover", (e) => {
-    // Highlight the mouseover target
-    e.target.style.backgroundColor = "black";
+  // Create 16 squares in each row
+  for (const row of rows) {
+    const widthSize = row.clientWidth / gridNum;
+    for (let i = 0; i < gridNum; i++) {
+      const square = document.createElement("div");
+      square.className = "square";
+      square.style.width = `${widthSize}px`;
+      square.style.height = "100%";
+      row.appendChild(square);
+    }
+  }
+};
 
-    // Reset the color after a short delay
-    setTimeout(() => {
-      e.target.style.backgroundColor = "";
-    }, 1000);
+const drawOnGrid = () => {
+  const squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("mouseover", (e) => {
+      // Highlight the mouseover target
+      e.target.style.backgroundColor = "black";
+
+      // Reset the color after a short delay
+      setTimeout(() => {
+        e.target.style.backgroundColor = "";
+      }, 1000);
+    });
   });
+};
+
+const playGrid = () => {
+  createGrid(gridNum);
+  drawOnGrid();
+};
+
+window.addEventListener("DOMContentLoaded", playGrid);
+button.addEventListener("click", () => {
+  container.innerHTML = "";
+  gridNum = Number(
+    prompt("Number of squares per side for the new grid (max 100)")
+  );
+
+  if (gridNum > 100) {
+    return;
+  }
+
+  playGrid();
 });
